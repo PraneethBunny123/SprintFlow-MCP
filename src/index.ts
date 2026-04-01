a3e758c17089ec32f363cf86c71efee3c733e1d2
praneeth <gp.gunturu@gmail.com>
2026-04-01 14:01:22 -0500

    Add new tool to get github repos based on username. Works in cursor and claude desktop

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,35 @@ server.tool("add-numbers",
   }
 )
 
+server.registerTool("get-github-repos", 
+  {
+    title: "Get GitHub Repos",
+    description: "Get the repositories of a GitHub user",
+    inputSchema: z.object({
+      username: z.string().describe("The username of the GitHub user"),
+    }),
+  }, 
+  async ({username}) => {
+    const repos = await fetch(`https://api.github.com/users/${username}/repos`, {
+      headers: {"User-Agent": "SprintFlow-MCP"}
+    })
+
+    if(!repos.ok) throw new Error("Github API Error");
+    const resData = await repos.json();
+
+    const repoList = resData.map((repo: any, i: number) => `${i+1}. ${repo.name}`).join("\n\n");
+    
+    return {
+      content: [
+        {
+          type: "text", 
+          text: `Github repos for ${username}: (${resData.length} repos): \n\n${repoList}`
+        }
+      ]
+    };
+  }
+)
+
 async function main() {
   const transport = new StdioServerTransport();
   await server.connect(transport);
