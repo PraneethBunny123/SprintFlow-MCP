91df5a4900bee67269ab9984537c3caf96cf1b0c
praneeth <gp.gunturu@gmail.com>
2026-04-09 20:26:14 -0400

    create compose-docker file

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { z } from "zod";
-import fs from "fs/promises";
-import path from "path";
-import { fileURLToPath } from "url";
 import { Project } from "./lib/types.js";
 
 // Create server instance
@@ -37,60 +34,6 @@ server.registerTool(
         {
           type: "text",
           text: JSON.stringify(project, null, 2)
-        }
-      ]
-    }
-  }
-)
-
-server.registerTool("get-github-repos", 
-  {
-    title: "Get GitHub Repos",
-    description: "Get the repositories of a GitHub user",
-    inputSchema: z.object({
-      username: z.string().describe("The username of the GitHub user"),
-    }),
-  }, 
-  async ({username}) => {
-    const repos = await fetch(`https://api.github.com/users/${username}/repos`, {
-      headers: {"User-Agent": "SprintFlow-MCP"}
-    })
-
-    if(!repos.ok) throw new Error("Github API Error");
-    const resData = await repos.json();
-
-    const repoList = resData.map((repo: any, i: number) => `${i+1}. ${repo.name}`).join("\n\n");
-    
-    return {
-      content: [
-        {
-          type: "text", 
-          text: `Github repos for ${username}: (${resData.length} repos): \n\n${repoList}`
-        }
-      ]
-    };
-  }
-)
-
-server.registerResource(
-  "system-policies",
-  "rules://all",
-  {
-    description: "resources for the system policies",
-    mimeType: "text/plain",
-  },
-  async (uri) => {
-    const __filename = fileURLToPath(import.meta.url);
-    const __dirname = path.dirname(__filename);
-    const rules = await fs.readFile(
-      path.resolve(__dirname, "../src/data/rules.doc"), "utf-8"
-    );
-    return {
-      contents: [
-        {
-          uri: uri.toString(),
-          mimeType: "text/plain",
-          text: rules,
         }
       ]
     }
